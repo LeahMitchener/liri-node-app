4bf90e32a2a8ab9ed79f0c4284677b4d28cc1209
LeahMitchener <46640686+LeahMitchener@users.noreply.github.com>
2019-05-11 22:16:04 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+console.log("This is loaded");
+exports.twitterKeys = {
+  consumer_key: "1NHSoxh157d8XbCmOlslRAln1",
+  consumer_secret: "NtTYLnSsWko8IQ2E39x631AgZryKMPoRtmBe22hBSWuerny1X3",
+  access_token_key: "3307128776-6ja3yJKzjVjEzO0xsCrFlmWQbyvxjm53Rb9L0iH",
+  access_token_secret: "DAjVEqH7tIAcquYcHMl4ihdCTeBdq3mgj9KNRfTO0y65X"
+};
